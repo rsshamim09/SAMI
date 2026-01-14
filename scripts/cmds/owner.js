52584be5728bdecadee98bf615f6e9492664430b
rsshamim09 <140725331+rsshamim09@users.noreply.github.com>
2026-01-14 10:15:39 +0600

    Update owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = {
 	config: {
 		name: "owner",
-		author: "ShAn",
+		author: "ShAMiM",
 		role: 0,
 		shortDescription: " ",
 		longDescription: "",
@@ -16,14 +16,14 @@ module.exports = {
 	onStart: async function ({ api, event }) {
 		try {
 			const ownerInfo = {
-				name: '𝑬𝒘𝑹 𝑺𝒉𝑨𝒏',
+				name: 'SH A MI M',
 				gender: '𝑴𝒂𝑳𝒆',
-				Birthday: '10-𝟎𝟕-𝟐𝟎𝟎5',
+				Birthday: '05-𝟎𝟕-𝟐𝟎08',
 				religion: '𝙄𝒔𝒍𝑨𝒎',
 				hobby: '𝑺𝒍𝒆𝒆𝑷𝒊𝒏𝑮',
-				Fb: 'https://www.facebook.com/Sh4n.Dev1',
+				Fb: 'https://www.facebook.com/share/1ENcLjFpgY/',
 				Relationship: '𝑺𝒊𝒏𝑮𝒆𝒍',
-				Height: '5"3'
+				Height: '5"11'
 			};
 
 			const bold = 'https://drive.google.com/uc?export=download&id=1J4yQ13L2WTpdOuqcP0yEmzULACdwfvnQ';
